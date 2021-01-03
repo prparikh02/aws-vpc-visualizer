9f5bd6414a0cda8c12c3d17f104da92e81554b4b
parth <prparikh02@gmail.com>
2021-01-03 13:15:32 -0500

    [Minor] Validate describe-security-groups and refactor

--- lambdas/api-backend/api_backend.py
+++ lambdas/api-backend/api_backend.py
@@ -19,23 +19,54 @@ def handler(event, context):
             query_params['roleArn'],
             query_params['region'],
         )
-        data = ec2_client.describe_security_groups()
+        api_response = ec2_client.describe_security_groups()
+        # TODO: Check API response for errors
+        security_groups = api_response['SecurityGroups']
+        try:
+            validate_security_groups(security_groups)
+        except ValueError as e:
+            print('Validation error: {}'.format(str(e)))
+            return make_response(500, {'messages': [str(e)]})
 
+    return make_response(200, body=api_response)
+
+
+def make_response(status_code, body={}):
     # Response format:
     # https://aws.amazon.com/premiumsupport/knowledge-center/malformed-502-api-gateway/
+    # Cannot add or remove top-level fields.
     return {
-        'statusCode': 200,
+        'statusCode': status_code,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',  # TODO: Remove this
         },
         'isBase64Encoded': False,
-        'body': json.dumps(data, separators=(',', ':')),
+        'body': json.dumps(body, separators=(',', ':')),
     }
 
 
-def get_security_groups(client):
-    pass
+def validate_security_groups(security_groups):
+    
+    def validate_rule(rule):
+        exclusive_fields = 0
+        if rule['IpRanges'] or rule['Ipv6Ranges']:
+            exclusive_fields += 1
+        if rule['UserIdGroupPairs']:
+            exclusive_fields += 1
+        if rule['PrefixListIds']:
+            exclusive_fields += 1
+
+            if exclusive_fields == 0:
+                raise ValueError('No mutually exclusive fields detected.')
+            if exclusive_fields > 1:
+                raise ValueError('More than two mutually exclusive fields detected.')
+
+    for security_group_info in security_groups:
+        for rule in security_group_info['IpPermissions']:
+            validate_rule(rule)
+        for rule in security_group_info['IpPermissionsEgress']:
+            validate_rule(rule)
 
 
 def create_client(service_name, role_arn, region):
